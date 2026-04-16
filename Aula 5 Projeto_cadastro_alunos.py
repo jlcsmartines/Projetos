780cda5984ccb7df20869a79101d319179d99d2b
jlcsmartines <joaolucas17032003@gmail.com>
2026-04-16 00:24:47 -0300

    concertei a funcao1 para adicionar alunos

--- Aula 5 Projeto_cadastro_alunos.py
+++ Aula 5 Projeto_cadastro_alunos.py
@@ -7,9 +7,9 @@
 # 6. Sair
 
 Alunos = {
-    'Nome': '', 
-    'Idade': 0,
-    'Nota': 0.0,
+    'Nome': [],
+    'Idade': [],
+    'Nota': [],
 }
 
 def menu():
@@ -21,9 +21,10 @@ def menu():
     print("6. Sair\n")
 
 def adicionar_aluno(nome, idade, nota):
-    Alunos["Nome"] = nome
-    Alunos["Idade"] = idade
-    Alunos["Nota"] = nota
+
+    Alunos['Nome'].append(nome)
+    Alunos['Idade'].append(idade)
+    Alunos['Nota'].append(nota)
 
 opcao = 0
 while opcao != 6:
@@ -40,10 +41,13 @@ while opcao != 6:
             
 
         case 2: #Listar todos os alunos
-            print(f"{Alunos}")
+            if Alunos["Nome"] == []:
+                print("\nNão existe nenhum aluno cadastrado.\n")
+            else:
+                print(f"\n{Alunos}")
                         
         case 3: #Buscar aluno pelo nome
-            print(f"{Alunos}")
+            print(f"Digite o nome do aluno: ")
             
         case 4: #Remover aluno
             print(f"{Alunos}")
